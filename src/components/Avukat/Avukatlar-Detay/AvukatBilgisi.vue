0407e9031722b4fd63f99c629f768199aa5126f0
Mustafa Kemal Karadeniz <108372741+M-kemal@users.noreply.github.com>
2023-10-02 19:43:02 +0300

    avukat kadrosu düzenlemesi

--- src/components/Avukat/Avukatlar-Detay/AvukatBilgisi.vue
+++ src/components/Avukat/Avukatlar-Detay/AvukatBilgisi.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>
-    <h2>Deneyim Detayları</h2>
-    <p>Telefon: {{ avukat.details.tel }}</p>
-    <p>Adres: {{ avukat.details.adres }}</p>
-    <p>Hakkında: {{ avukat.details.hakkında }}</p>
-    <p>{{ avukat }}</p>
+  <div class="myCard">
+    <div class="flex items-start justify-center p-6 h-[30rem]">
+      <img
+        :src="require(`../../../assets/${avukat.img}.jpeg`)"
+        :alt="avukat.name"
+        class="w-full h-full object-cover mr-4 shadow rounded-md"
+      />
+
+      <div
+        v-for="avk in avukat.details"
+        :key="avk.id"
+        class="flex flex-col justify-center items-start"
+      >
+        <h1 class="heading1 !text-black">Av.{{ avk.name }}</h1>
+        <div class="text-base">
+          <span class="heading4 !mb-1 !text-black !font-semibold">Tel :</span>
+          <span>{{ avk.tel }}</span>
+        </div>
+        <div class="text-base">
+          <span class="heading4 mr-1 !mb-1 !text-black !font-semibold"
+            >Mail :</span
+          >
+          <span>{{ avk.mail }}</span>
+        </div>
+        <div class="text-base">
+          <span class="heading4 mr-1 !mb-1 !text-black !font-semibold"
+            >Adres :</span
+          >
+          <span>{{ avk.adres }}</span>
+        </div>
+        <div class="leading-relaxed text-base">
+          <span class="heading4 mr-1 !mb-1 !text-black !font-semibold"
+            >Hakkında :</span
+          >
+          <span>{{ avk.hakkında }}</span>
+        </div>
+        <!-- <p>{{ avukat }}</p> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,3 +59,43 @@ export default {
   },
 };
 </script>
+
+<style>
+.myCard {
+  border: none;
+  border-radius: 10px;
+  background: linear-gradient(
+    to right,
+    #77530a,
+    #ffd277,
+    #77530a,
+    #77530a,
+    #ffd277,
+    #77530a
+  );
+  background-size: 250%;
+  background-position: left;
+  color: #020201;
+  position: relative;
+  display: flex;
+  /* align-items: center; */
+  justify-content: center;
+
+  transition-duration: 1s;
+  overflow: hidden;
+}
+.myCard::before {
+  position: absolute;
+  /* content: "BUTTON"; */
+  color: #ffd277;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 97%;
+  height: 90%;
+  border-radius: 8px;
+  transition-duration: 1s;
+  background-color: rgba(0, 0, 0, 0.842);
+  background-size: 200%;
+}
+</style>
--- src/components/Avukat/Avukatlar-Detay/AvukatlarListesi.vue
+++ src/components/Avukat/Avukatlar-Detay/AvukatlarListesi.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
+  <div class="bg-black text-white">
+    <HeaderApp />
+    <div class="CostumContainer h-full">
+      <h2 class="heading1 !text-firstColor !text-center mt-10">
+        MHK Hukuk Avukat Kadromuz
+      </h2>
+      <div class="flex items-start justify-between space-x-2 py-10 h-full">
+        <div class="w-1/4">
+          <div v-for="avukat in avukatlar" :key="avukat.slug" class="">
+            <router-link
+              :to="{ name: 'AvukatBilgisi', params: { slug: avukat.slug } }"
+              class="flex flex-col mb-4 relative"
+            >
+              <button
+                class="btn font-header !py-8 text-xl z-40"
+                :data-text="`Av.${avukat.name}`"
+              >
+                <img
+                  :src="require(`../../../assets/${avukat.img}.jpeg`)"
+                  :alt="avukat.name"
+                  class="w-12 h-12 rounded-lg border-2 border-yellow-900 absolute top-2 left-2"
+                />
+              </button>
+            </router-link>
+          </div>
+        </div>
+        <div class="w-3/4 flex">
+          <router-view :key="$route.path"></router-view>
+        </div>
+      </div>
+      <FooterApp brand="MHK Hukuk" year="2023" developer="mkemal.krdnz">
+        <template #icon>
+          <LawIcon width="24" height="24" fill="#e1b254" />
+        </template>
+      </FooterApp>
+    </div>
+
     <ul>
-      <li v-for="avukat in avukatlar" :key="avukat.slug">
+      <!-- <li v-for="avukat in avukatlar" :key="avukat.slug">
         <router-link
           :to="{ name: 'AvukatBilgisi', params: { slug: avukat.slug } }"
         >
           {{ avukat.name }} - {{ avukat.job }}
         </router-link>
-      </li>
+      </li> -->
     </ul>
-    <router-view :key="$route.path"></router-view>
   </div>
 </template>
 
@@ -18,12 +53,14 @@ import { useRouter } from "vue-router";
 import { ref, onMounted } from "vue";
 
 import jsonData from "@/components/person.json";
+import HeaderApp from "../HeaderApp.vue";
+import FooterApp from "@/components/Footer/FooterApp.vue";
+import LawIcon from "@/components/icons/LawIcon.vue";
 
 export default {
   name: "AvukatlarListesi",
   setup() {
     const avukatlar = ref(jsonData.avukatlar);
-
     // İlk slug verisini yükle
     const router = useRouter();
     const loadInitialSlug = () => {
@@ -32,15 +69,14 @@ export default {
         router.push({ name: "AvukatBilgisi", params: { slug: firstSlug } });
       }
     };
-
     onMounted(() => {
       loadInitialSlug();
     });
-
     return {
       avukatlar,
     };
   },
+  components: { HeaderApp, FooterApp, LawIcon },
 };
 </script>
 
